2fbb16d0b9ab92e8bc1e4afcd18c3deaf57d93aa
saltify7 <91529939+saltify7@users.noreply.github.com>
2026-01-21 10:51:43 +0000

    Bump version

--- caido.config.ts
+++ caido.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   id,
   name: "Project Minify",
   description: "Reduce project size by transferring data into another project.",
-  version: "0.1.1",
+  version: "0.1.2",
   author: {
     name: "Saltify",
     email: "saltify7@gmail.com",
